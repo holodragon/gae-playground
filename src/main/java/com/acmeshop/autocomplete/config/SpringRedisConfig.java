5b1a484543f8e3290ee3abcac0268bbc3887f8f7
Administrator <admin@example.com>
2017-12-02 22:24:31 +0800

    commit for parameter extraction

--- src/main/java/com/acmeshop/autocomplete/config/SpringRedisConfig.java
+++ src/main/java/com/acmeshop/autocomplete/config/SpringRedisConfig.java
@@ -1,34 +1,54 @@
 package com.acmeshop.autocomplete.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class SpringRedisConfig {
+	@Value("${autocomplete.store.redis.host}")
+	private String redisHost;
+	@Value("${autocomplete.store.redis.port}")
+	private int redisPort;
+
+	public String getRedisHost() {
+		return redisHost;
+	}
+
+	public void setRedisHost(String redisHost) {
+		this.redisHost = redisHost;
+	}
+
+	public int getRedisPort() {
+		return redisPort;
+	}
+
+	public void setRedisPort(int redisPort) {
+		this.redisPort = redisPort;
+	}
+
 	@Bean
 	public JedisConnectionFactory connectionFactory() {
 		JedisConnectionFactory connectionFactory = new JedisConnectionFactory();
-		connectionFactory.setHostName("localhost");
-		connectionFactory.setPort(6379);
+		connectionFactory.setHostName(redisHost);
+		connectionFactory.setPort(redisPort);
 		return connectionFactory;
 	}
 
-//	@Bean
-//	public RedisTemplate<String, Object> redisTemplate() {
-//		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
-//		redisTemplate.setConnectionFactory(connectionFactory());
-//		redisTemplate.setKeySerializer(new StringRedisSerializer());
-//		return redisTemplate;
-//	}
-//
-//	@Bean
-//	public StringRedisTemplate strRedisTemplate() {
-//		StringRedisTemplate redisTemplate = new StringRedisTemplate();
-//		redisTemplate.setConnectionFactory(connectionFactory());
-//		return redisTemplate;
-//	}
+	// @Bean
+	// public RedisTemplate<String, Object> redisTemplate() {
+	// RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String,
+	// Object>();
+	// redisTemplate.setConnectionFactory(connectionFactory());
+	// redisTemplate.setKeySerializer(new StringRedisSerializer());
+	// return redisTemplate;
+	// }
+	//
+	// @Bean
+	// public StringRedisTemplate strRedisTemplate() {
+	// StringRedisTemplate redisTemplate = new StringRedisTemplate();
+	// redisTemplate.setConnectionFactory(connectionFactory());
+	// return redisTemplate;
+	// }
 }
