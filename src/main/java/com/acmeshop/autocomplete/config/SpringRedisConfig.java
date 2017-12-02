28fd2551dabdfbd9e1db65ef27693bf7e988495b
Administrator <admin@example.com>
2017-12-02 23:41:00 +0800

    commit for properties reference fix

--- src/main/java/com/acmeshop/autocomplete/config/SpringRedisConfig.java
+++ src/main/java/com/acmeshop/autocomplete/config/SpringRedisConfig.java
@@ -1,40 +1,33 @@
 package com.acmeshop.autocomplete.config;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 
+import com.acmeshop.autocomplete.loader.ProductJsonLoader;
+
 @Configuration
 public class SpringRedisConfig {
+
+	private Log log = LogFactory.getLog(SpringRedisConfig.class);
+
+	
 	@Value("${autocomplete.store.redis.host}")
 	private String redisHost;
 	@Value("${autocomplete.store.redis.port}")
 	private int redisPort;
-	@Value("${autocomplete.store.redis.password}")
+	@Value("${autocomplete.store.redis.password:#{null}}")
 	private String redisPassword;
-
-	public String getRedisHost() {
-		return redisHost;
-	}
-
-	public void setRedisHost(String redisHost) {
-		this.redisHost = redisHost;
-	}
-
-	public int getRedisPort() {
-		return redisPort;
-	}
-
-	public void setRedisPort(int redisPort) {
-		this.redisPort = redisPort;
-	}
 
 	@Bean
 	public JedisConnectionFactory connectionFactory() {
 		JedisConnectionFactory connectionFactory = new JedisConnectionFactory();
 		connectionFactory.setHostName(redisHost);
 		connectionFactory.setPort(redisPort);
+		log.info(redisPassword);
 		connectionFactory.setPassword(redisPassword);
 		return connectionFactory;
 	}
